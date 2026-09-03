4985783d96dc4381a062042f05dcdd5acb41d647
Omar Khair <omar.khair70@gmail.com>
2026-09-03 14:08:58 +0300

    infra(oci): expose optional foundation service outputs

--- infra/oci/terraform/outputs.tf
+++ infra/oci/terraform/outputs.tf
@@ -22,3 +22,24 @@ output "private_data_subnet_id" {
   value       = oci_core_subnet.private_data.id
   description = "Teswa private data subnet OCID."
 }
+
+
+output "media_bucket_name" {
+  value       = var.enable_object_storage ? oci_objectstorage_bucket.media[0].name : null
+  description = "Private Teswa media bucket name when enabled."
+}
+
+output "backups_bucket_name" {
+  value       = var.enable_object_storage ? oci_objectstorage_bucket.backups[0].name : null
+  description = "Private versioned Teswa backups bucket name when enabled."
+}
+
+output "teswa_vault_id" {
+  value       = var.enable_vault ? oci_kms_vault.teswa[0].id : null
+  description = "Default Teswa Vault OCID when enabled."
+}
+
+output "ops_notification_topic_id" {
+  value       = var.enable_notifications ? oci_ons_notification_topic.teswa_ops[0].topic_id : null
+  description = "Teswa operations notification topic OCID when enabled."
+}
