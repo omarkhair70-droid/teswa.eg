7bc584a95b1f94d81b4aefc860e429273868bb3d
Omar Khair <omar.khair70@gmail.com>
2026-09-03 13:19:13 +0300

    infra(oci): scope platform services to Teswa compartment

--- infra/oci/terraform/platform-services.tf
+++ infra/oci/terraform/platform-services.tf
@@ -1,10 +1,10 @@
 data "oci_objectstorage_namespace" "teswa" {
-  compartment_id = var.compartment_ocid
+  compartment_id = var.tenancy_ocid
 }
 
 resource "oci_objectstorage_bucket" "media" {
   count          = var.enable_object_storage ? 1 : 0
-  compartment_id = var.compartment_ocid
+  compartment_id = oci_identity_compartment.teswa.id
   namespace      = data.oci_objectstorage_namespace.teswa.namespace
   name           = "teswa-media"
   access_type    = "NoPublicAccess"
@@ -14,7 +14,7 @@ resource "oci_objectstorage_bucket" "media" {
 
 resource "oci_objectstorage_bucket" "backups" {
   count          = var.enable_object_storage ? 1 : 0
-  compartment_id = var.compartment_ocid
+  compartment_id = oci_identity_compartment.teswa.id
   namespace      = data.oci_objectstorage_namespace.teswa.namespace
   name           = "teswa-backups"
   access_type    = "NoPublicAccess"
@@ -24,7 +24,7 @@ resource "oci_objectstorage_bucket" "backups" {
 
 resource "oci_kms_vault" "teswa" {
   count          = var.enable_vault ? 1 : 0
-  compartment_id = var.compartment_ocid
+  compartment_id = oci_identity_compartment.teswa.id
   display_name   = "teswa-vault"
   vault_type     = "DEFAULT"
   freeform_tags  = var.freeform_tags
@@ -32,7 +32,7 @@ resource "oci_kms_vault" "teswa" {
 
 resource "oci_ons_notification_topic" "teswa_ops" {
   count          = var.enable_notifications ? 1 : 0
-  compartment_id = var.compartment_ocid
+  compartment_id = oci_identity_compartment.teswa.id
   name           = "teswa-ops"
   description    = "Teswa OCI operational alerts"
   freeform_tags  = var.freeform_tags
