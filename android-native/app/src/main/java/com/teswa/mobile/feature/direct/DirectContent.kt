4e8dc98faad7d95d67bddf2616091e96169912ea
Omar Khair <omar.khair70@gmail.com>
2026-09-21 07:52:54 +0300

    Fix Direct attachment fullscreen viewer import

--- android-native/app/src/main/java/com/teswa/mobile/feature/direct/DirectContent.kt
+++ android-native/app/src/main/java/com/teswa/mobile/feature/direct/DirectContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
