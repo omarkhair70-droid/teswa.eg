181819c059ff2db90d375e884a14945220e2d373
Omar Khair <omar.khair70@gmail.com>
2026-09-21 08:09:23 +0300

    Fix contextual origin viewer imports

--- android-native/app/src/main/java/com/teswa/mobile/feature/contextual/ContextualContent.kt
+++ android-native/app/src/main/java/com/teswa/mobile/feature/contextual/ContextualContent.kt
@@ -3,8 +3,10 @@ package com.teswa.mobile.feature.contextual
 import android.content.Intent
 import android.net.Uri
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -19,6 +21,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
